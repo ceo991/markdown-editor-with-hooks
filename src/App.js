aed7085d3d1a4aac134c6d3be0384cd7911dcf75
ceo991 <ceozen91@gmail.com>
2022-06-25 16:04:29 +0300

    mobile responsiveness added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
-import  Split  from 'react-split';
+import Split  from 'react-split';
 import Editor from './components/Editor';
 import Preview from './components/Preview';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   const [content, setContent] = useState("# h1\n\n## h2\n\n[Google](https://google.com)\n\n```\n\n function(){//this is a function}\n\n ``` \n\n`Array.prototype.map()` \n\n- list item\n\n> a block quote\n\n![The San Juan Mountains are beautiful!](https://live.staticflickr.com/389/31833779864_7ec0b63ffc_h.jpg 'San Juan Mountains') \n\n **a bolded text**")
   const [displayMode, setDisplayMode] = useState("default")
+  const[width,setWidth] = useState(window.innerWidth)
+  const [direction,setDirecton]=useState()
+  const [split,setSpilt]=useState()
 
+  useEffect(() => {
+    window.addEventListener("resize", () => {setWidth(window.innerWidth)})
+  },[])
+
+  useEffect(() => {
+    setDirecton(width <= 700 ? "vertical" : "horizontal")
+    setSpilt(width <= 700 ? "split-mobile" : "split")
+  },[width])
 
   const handleOnChange = (content)=>{
     setContent(content)
@@ -26,8 +37,8 @@ function App() {
       {displayMode === "default" ?
           <Split
             sizes={[60, 40]} 
-            direction="horizontal" 
-            className="split"
+            direction={direction} 
+            className={split}
           >
               <Editor content={content} changeState={handleOnChange} changeDisplayMode={changeDisplayMode}/>
               <Preview content={content} changeDisplayMode={changeDisplayMode}/>
